418d7ef91facc6b81c295305c38c68e5ddbe0ab2
Jamal <jwest23@ufl.edu>
2018-06-13 22:02:59 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component'
+import { EventService } from './events/shared/event.service'
+import { EventDetailsComponent } from './events/event-details/event-details.component'
+import { appRoutes } from './routes'
+
+import { ToastrService } from './common/toastr.service';
 
 
-import { AppComponent } from './app.component';
-
-
-@NgModule({
+@NgModule({ imports: [
+  BrowserModule,
+  RouterModule.forRoot(appRoutes)
+],
   declarations: [
-    AppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
+    EventDetailsComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [EventService],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core'
+import { EventService } from './shared/event.service';
+import { ToastrService } from '../common/toastr.service'
+
+declare let toastr
+@Component ({
+    template:`
+    <div>
+        <h1>Upcoming Angular Events</h1>
+        <hr/>
+        <div class="row">
+            <div *ngFor="let event of events" class="col-md-5">
+            <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
+    </div>
+`,
+
+    
+    
+})
+export class EventsListComponent implements OnInit {
+    events:any[]
+
+    constructor(private eventService: EventService){
+    
+    }
+
+    ngOnInit(){
+        this.events = this.eventService.getEvents()
+    }
+
+    handleThumbnailClick(eventName){
+        toastr.success(eventName)
+    }
+}
